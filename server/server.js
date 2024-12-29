5344e45c5f6d39f2dc43e9a7022f3efd054d85e0
chamathishanka <pkchamath2000@gmail.com>
2024-12-29 16:37:11 +0530

    Integrate MongoDB connection, Refactor server setup to use ES modules, add dotenv for environment variables

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 const app = express();
-const port = 3000;
+dotenv.config();  // Ensure this line is correctly executed
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+const PORT = process.env.PORT || 8000;
+const MongoURL = process.env.MONGO_URL;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+mongoose.connect(MongoURL).then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
 });
